396d34b96dee445a4aee4ba4b76a861a33825913
joerow <joerow@gmail.com>
2022-04-02 11:09:01 +0800

    add playRound functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//returns the randomly selected computer choice
 function computerPlay(){
     let computerChoice="default";
     let randomNumber = Math.floor(Math.random()*100) + 1;
@@ -21,4 +22,27 @@ function computerPlay(){
     console.log(computerChoice);
     return computerChoice;
 }
-computerPlay();
+
+//returns the result of the round
+function playRound(playerSelection, computerSelection) {
+    switch(true){
+        case playerSelection==computerSelection:
+            return "you both picked the same so that was a draw";
+        case playerSelection=="rock" && computerSelection =="scissors":
+            return "You picked rock and they picked scissors, you win";
+        case playerSelection=="rock" && computerSelection =="paper":
+            return "You picked rock and they picked paper, you lose";
+        case playerSelection=="paper" && computerSelection =="scissors":
+            return "You picked paper and they picked scissors, you lose";
+        case playerSelection=="paper" && computerSelection =="rock":
+            return "You picked paper and they picked rock, you win";
+        case playerSelection=="scissors" && computerSelection =="paper":
+            return "You picked scissors and they picked paper, you win";
+        case playerSelection=="scissors" && computerSelection =="rock":
+            return "You picked scissors and they picked rock, you lose";
+    }
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
